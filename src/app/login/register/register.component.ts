6bccdbc8f8218a90c1ff165ba0ab747b1b08c8a9
RicardoMatiasPriou <74669252+RicardoMatiasPriou@users.noreply.github.com>
2022-06-24 15:38:07 -0300

    1818

--- src/app/login/register/register.component.ts
+++ src/app/login/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { Alumno } from '../../dashboard/models/alumno';
+import { AlumnosService } from '../../dashboard/service/alumnos-service.service';
 
 @Component({
   selector: 'app-register',
@@ -12,27 +14,46 @@ export class RegisterComponent implements OnInit {
     email: '',
     password: '',
   };
-
+  alumnosList:Alumno[]
   constructor(
     private AuthService: AuthService,
     private route: ActivatedRoute,
-    private router: Router
-  ) {}
+    private router: Router,
+    private AlumnosService:AlumnosService
+  ) { }
 
-  ngOnInit(): void {
-    // this.route.queryParams.subscribe((params) => {
-    //   this.name = params['name'];
-    // });
+  ngOnInit(
+  ): void {
+    this.getUsers()
   }
   Registrarse() {
     const { email, password } = this.usuario;
 
     this.AuthService.register(email, password).then((res) => {
       this.AuthService.login(email, password).then((r) => {
+        localStorage.setItem('log', 'true');
+
+        this.alumnosList.forEach((e) => {
+          if (r?.user?.email == e.email) {
+            localStorage.setItem('reg', 'true');
+            console.log(true);
+
+            if (e.admin == true) {
+              console.log(true);
+
+              localStorage.setItem('admin', 'true');
+            }
+          }
+        });
         this.router.navigate(['/dashboard']);
       });
-    });
+    })
   }
 
+  getUsers() {
+    this.AlumnosService.getAlumnosList().subscribe((data) => {
+      this.alumnosList = data;
+    });
+  }
 
 }
